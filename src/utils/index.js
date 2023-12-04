5729114dc105a627e2f0e00bca032fdd6d4b98ec
juanjomayotte <juanjomayotte@gmail.com>
2023-12-04 13:16:16 +0100

    Updates

--- src/utils/index.js
+++
@@ -1,8 +0,0 @@
-/* param (array) products cartProduct
-returns {number} total Price */
-
-export const totalPrice = (products) => {
-    let sum = 0;
-    products.forEach((product) => (sum += product.price));
-    return parseFloat(sum.toFixed(2));
-}
